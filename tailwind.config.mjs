f03ef4320483c61b30356f5307e7cf40484a402f
Majora <samydahi2@gmail.com>
2024-01-18 17:43:02 +0100

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				'laravel-red': 'red',
+				'react-blue': 'rgb(97, 219, 251)',
+			}
+		},
+	},
+	plugins: [],
+}
